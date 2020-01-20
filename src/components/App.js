c6034aec21bb01466b18ca52397a314af61e153e
danielmitiku <wedanielmitiku@gmail.com>
2020-01-20 16:39:08 +0300

    implemented App.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id='container'>
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
+}
+
+export default App;

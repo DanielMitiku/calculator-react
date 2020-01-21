2e814374f055d6cd0ca47af850030c37917be80d
danielmitiku <wedanielmitiku@gmail.com>
2020-01-21 16:05:30 +0300

    added initial calculate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,6 +1,28 @@
-const calculate = (calculator, bName) => {
-  if(calculator.total !== null && calculator.next !== null && calculator.operation !== null) {
+import operate from './operate';
 
+const calculate = ({ total, next, operation }, bName) => {
+  if(total !== null && next !== null && operation !== null) {
+    if(isNaN(bName)) {
+      if(bName === '+/-') {
+        return {
+          total: parseFloat(total) ? (-1 * total).toString() : total,
+          next: parseFloat(next) ? (-1 * next).toString() : next,
+          operation: ''
+        }
+      } else if (bName === '+' || bName === '-' || bName === 'X' || bName === '÷') {
+        return {
+          total: next,
+          next: '',
+          operation: bName 
+        }
+      }
+    }else {
+      return {
+        total: total + bName,
+        next: next + bName,
+        operation: ''
+      }
+    }
   } else {
     return false;
   }

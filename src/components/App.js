480c607a96a9ba3699dd429d6b3ecee704e1b22f
danielmitiku <wedanielmitiku@gmail.com>
2020-01-23 11:28:30 +0300

    added states and handleClick function

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ButtonPanel from './ButtonPannel';
 import Display from './Display';
-// import calculate from '../logic/calculate';
+import calculate from '../logic/calculate';
 import '../styles/App.css';
 
-const App = () => (
-  <div id="container">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(bName) {
+    const calResult = calculate(this.state, bName);
+    this.setState(calResult);
+  }
+
+  render() {
+    return (
+      <div id="container">
+        <Display total={this.state.total} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+}
 
 export default App;
